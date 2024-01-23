41c506a858996ebc36c2327eade4fae9b9d2ed7c
Walter-Britos1 <walterbritos418@gmail.com>
2024-01-23 01:47:53 -0300

    add routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,8 @@ const Main = () => {
     <View style={{ flex: 1 }}>
       <AppBar />
       <Routes>
-        <Route exact path="/" element={<ReposList />} />
+      <Route exact path="/" element={<ReposList />} />
+        <Route exact path="/registrer" element={<Text>Registrer</Text>} />
         <Route exact path="/myprofile" element={<Text>My Profile</Text>} />
         <Route exact path="/about" element={<Text>About Us</Text>} />
         <Route exact path="/signin" element={<Text>Working in it</Text>} />
