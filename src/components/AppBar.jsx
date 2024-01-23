e475b8b3172f5cbf91e3ce6e80f2f4803787a966
walterbritos418 <walterbritos418@gmail.com>
2024-01-23 05:02:05 +0000

    add routes from idx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -6,8 +6,8 @@ import AppBarTab from "./AppBarTab";
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <AppBarTab active to='/'>Repositories</AppBarTab>
+      <ScrollView horizontal style={styles.scroll}>
+        <AppBarTab active to='/repositories'>Repositories</AppBarTab>
         <AppBarTab active to='/registrer'>Register</AppBarTab>
         <AppBarTab active to='/myprofile'>My Profile</AppBarTab>
         <AppBarTab active to='/about'>About Us</AppBarTab>
@@ -23,8 +23,10 @@ const styles = StyleSheet.create({
     backgroundColor: theme.appBar.primary,
     flexDirection: "row",
     padding: Constants.statusBarHeight + 10,
-    paddingBottom: 10,
   },
+  scroll: {
+    paddingBottom: 15,
+  }
 })
 
 export default AppBar;
