8ad48421c479d0598099de64fe1893b896301220
Walter-Britos1 <walterbritos418@gmail.com>
2024-01-23 01:22:24 -0300

    add routes

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -6,8 +6,14 @@ import AppBarTab from "./AppBarTab";
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab active to='/'>Repositories</AppBarTab>
-      <AppBarTab active to='/signin'>Sign in</AppBarTab>
+      <ScrollView horizontal style={styles.scroll}>
+        <AppBarTab active to='/'>Repositories</AppBarTab>
+        <AppBarTab active to='/registrer'>Register</AppBarTab>
+        <AppBarTab active to='/myprofile'>My Profile</AppBarTab>
+        <AppBarTab active to='/about'>About Us</AppBarTab>
+        <AppBarTab active to='/signin'>Sign in</AppBarTab>
+        <AppBarTab active to="/">Exit</AppBarTab>
+      </ScrollView>
     </View>
   );
 };
