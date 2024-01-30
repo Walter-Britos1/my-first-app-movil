86923ded6dd6b6a637ec1b41bfb29f32349de501
Walter-Britos1 <walterbritos418@gmail.com>
2024-01-30 01:39:57 -0300

    created "StyldTextInput"

---
+++ src/components/StyledTextInput.jsx
@@ -0,0 +1,25 @@
+import { TextInput, StyleSheet } from "react-native";
+
+const StyledTetxtInput = ({ style = {}, ...props}) => {
+  const inputStyle = {
+    ...styles.textInput,
+    ...style
+  }
+
+  return(
+    <TextInput style={ inputStyle } {...props}></TextInput>
+  )
+};
+
+const styles = StyleSheet.create({
+  textInput: {
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#999',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginBottom: 10
+  }
+})
+
+export default StyledTetxtInput;
