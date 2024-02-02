956eab607e2f674cc5d0259924cf2b3228d2b960
Walter-Britos1 <walterbritos418@gmail.com>
2024-02-02 00:12:48 -0300

    getting list of repositories from backend

--- src/components/ReposList.jsx
+++ src/components/ReposList.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import { FlatList, View, Text } from "react-native";
-import repositories from "../data/repositories.js";
 import RepositoryItems from "./RepositoryItems.jsx";
 
 const ReposList = () => {
+  const [repositories, setRepositories] = useState(null);
+
+  const repositoriesFetch = async () => {
+    const response = await fetch("http://localhost:5000/api/repositories");
+    const json = await response.json();
+    setRepositories(json)
+  };
+
+  useEffect(() => {
+    repositoriesFetch()
+  }, [])
+
+  const repositoriesList = repositories
+      ? repositories.edges.map(edge => edge.node)
+      : [];
+
   return (
     <FlatList
-      data={repositories}
+      data={repositoriesList}
       ItemSeparatorComponent={()=> <Text> </Text>}
       renderItem={(({item: repo}) => (
         <RepositoryItems {...repo}/>
