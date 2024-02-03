8d1b589243323bbed9707c05daaff154e7f8d2b7
Walter-Britos1 <walterbritos418@gmail.com>
2024-02-02 22:15:06 -0300

    created hooks

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+const useRepositorieList = () => {
+  const [repositories, setRepositories] = useState(null);
+
+  const repositoriesFetch = async () => {
+    const response = await fetch("http://localhost:5000/api/repositories");
+    const json = await response.json();
+    setRepositories(json)
+  };
+
+  useEffect(() => {
+    repositoriesFetch()
+  }, []);
+
+  const repositoriesList = repositories
+  ? repositories.edges.map(edge => edge.node)
+  : [];
+
+  return { respositories: repositoriesList }
+};
+
+export default useRepositorieList;
