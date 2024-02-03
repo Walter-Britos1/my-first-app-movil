bc263bfd5bcdb589a2e4609eaa81bd153cce2fe7
Walter-Britos1 <walterbritos418@gmail.com>
2024-02-03 00:06:17 -0300

    change config

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 
 const useRepositorieList = () => {
   const [repositories, setRepositories] = useState(null);
-
+ 
   const repositoriesFetch = async () => {
     const response = await fetch("http://localhost:5000/api/repositories");
     const json = await response.json();
     setRepositories(json)
   };
-
+  
   useEffect(() => {
     repositoriesFetch()
   }, []);
@@ -17,7 +17,7 @@ const useRepositorieList = () => {
   ? repositories.edges.map(edge => edge.node)
   : [];
 
-  return { respositories: repositoriesList }
+  return { loading, repositories: repositoriesList, refetch };
 };
 
 export default useRepositorieList;
